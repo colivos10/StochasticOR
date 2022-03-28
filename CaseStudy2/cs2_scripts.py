62079dd69582050d995f597dbaf077b7b55a6dd1
Carlos Olivos <cro0010@auburn.edu>
2022-03-28 17:31:09 -0500

    Simulation performs with multiprocessors
    Signed-off-by: Carlos Olivos <cro0010@auburn.edu>

--- CaseStudy2/cs2_scripts.py
+++ CaseStudy2/cs2_scripts.py
@@ -1,18 +1,63 @@
 import numpy as np
+from sympy.abc import t
+
 
 def log_normal_variates(mu, sigma, replicates):
+    u1 = np.random.uniform()
+    u2 = np.random.uniform()
 
-    x_log = []
-    for i in range(replicates):
-        u1 = np.random.uniform()
-        u2 = np.random.uniform()
+    # box miller
+    z1 = np.sqrt(-2 * np.log(u1)) * np.cos(2 * np.pi * u2)
+    z2 = np.sqrt(-2 * np.log(u1)) * np.sin(2 * np.pi * u2)
 
-        #box miller
-        z1 = np.sqrt(-2 * np.log(u1)) * np.cos(2 * np.pi * u2)
-        z2 = np.sqrt(-2 * np.log(u1)) * np.sin(2 * np.pi * u2)
+    # log normal
+    x = np.exp(mu + sigma * np.random.choice([z1, z2]))
 
-        # log normal
-        x = np.exp(mu + sigma * np.random.choice([z1, z2]))
-        x_log.append(x)
+    return x
 
-    return np.mean(x_log), np.var(x_log)
+def non_homogenous_pp(replicates, formula, lambda_star):
+    times_vector = []
+    time = 0
+    uniform_variate = np.random.uniform()  # Uniform(0,1)
+    time_arrival_variate = - np.log(1 - uniform_variate) / lambda_star  # x_n
+    time = time + time_arrival_variate  # t = t + x_n
+    while time <= 24:
+        N = 0
+
+        uniform_variate_hat = np.random.uniform()  # Uhat Uniform(0,1)
+        lambda_t_return = formula.subs(t, time)
+
+        if uniform_variate_hat <= lambda_t_return / lambda_star:
+            N = N + 1
+            times_vector.append(time)
+
+        uniform_variate = np.random.uniform()  # Uniform(0,1)
+        time_arrival_variate = - np.log(1 - uniform_variate) / lambda_star  # x_n
+        time = time + time_arrival_variate  # t = t + x_n
+
+    return len(times_vector)
+
+
+def non_homogenous_compound_pp(replicates, formula, lambda_star):
+    times_vector = []
+    email_size = []
+    time = 0
+    uniform_variate = np.random.uniform()  # Uniform(0,1)
+    time_arrival_variate = - np.log(1 - uniform_variate) / lambda_star  # x_n
+    time = time + time_arrival_variate  # t = t + x_n
+    while time <= 24:
+        N = 0
+
+        uniform_variate_hat = np.random.uniform()  # Uhat Uniform(0,1)
+        lambda_t_return = formula.subs(t, time)
+
+        if uniform_variate_hat <= lambda_t_return / lambda_star:
+            N = N + 1
+            times_vector.append(time)
+            email_size.append(log_normal_variates(4, 1, 1))
+
+        uniform_variate = np.random.uniform()  # Uniform(0,1)
+        time_arrival_variate = - np.log(1 - uniform_variate) / lambda_star  # x_n
+        time = time + time_arrival_variate  # t = t + x_n
+
+    return times_vector, email_size
--- CaseStudy2/mails_simulation.py
+++ CaseStudy2/mails_simulation.py
@@ -1,71 +1,60 @@
 # %%
-import numpy as np
 import matplotlib.pyplot as plt
-from sympy import *
+import numpy as np
+from joblib import Parallel, delayed
+from sympy import Piecewise
 from sympy.abc import t
-from sympy.plotting import plot
-# %%
-z_normals = []
-x_log = []
 
-for i in range(10000):
-    u1 = np.random.uniform()
-    u2 = np.random.uniform()
+from CaseStudy2 import cs2_scripts as cs2
 
-    #box miller
-    z1 = np.sqrt(-2 * np.log(u1)) * np.cos(2 * np.pi * u2)
-    z2 = np.sqrt(-2 * np.log(u1)) * np.sin(2 * np.pi * u2)
+# %% Number 2
 
-    # log normal
-    mu = 4
-    sigma = 1
+size_emails = Parallel(n_jobs=8)(delayed(cs2.log_normal_variates)(4, 1, i) for i in range(10000))
+print(f'The mean of the size of the emails is: {np.mean(size_emails)}')
+print(f'The variance of the size of the emails is: {np.var(size_emails)}')
 
-    x = np.exp(mu + sigma * np.random.choice([z1, z2]))
-    x_log.append(x)
+# %%  Number 3
 
-print(np.mean(x_log), np.var(x_log))
+lambda_t = np.array([1, 1, 1, 1, 1, 1, 2, 5, 5, 5, 3, 8, 10, 10, 10, 8, 6, 4, 3, 2, 1, 1, 1, 1, 1])
+l = np.linspace(0, 24, 100)
+steps_piece_wise_function = [((l >= 0) & (l <= 5)), ((l > 5) & (l <= 6)), ((l > 6) & (l <= 7)), ((l > 7) & (l <= 9)),
+                             ((l > 9) & (l <= 10)), ((l > 10) & (l <= 11)), ((l > 11) & (l <= 12)),
+                             ((l > 12) & (l <= 14)), ((l > 14) & (l <= 17)), ((l > 17) & (l <= 20)),
+                             ((l > 20) & (l <= 24))]
 
-# Poisson
-lambda_t = np.array([1, 1, 1, 1, 1, 2, 5, 5, 5, 3, 8, 10, 10, 10, 8, 6, 4, 3, 2, 1, 1, 1, 1, 1])
-plt.scatter([i for i in range(24)], lambda_t)
+return_lambda_function = [lambda l: 1, lambda l: l - 4, lambda l: 3 * l - 16, lambda l: 5,
+                          lambda l: -2 * l + 23, lambda l: 5 * l - 47, lambda l: 2 * l - 14, lambda l: 10,
+                          lambda l: -2 * l + 38, lambda l: -l + 21, lambda l: 1]
+
+plt.scatter([i for i in range(0, 25)], lambda_t)
+plt.plot(np.linspace(0, 24, 100), np.piecewise(l, steps_piece_wise_function, return_lambda_function))
 plt.ylim(ymin=0)
-plt.xlim(xmin=0)
+plt.xlim(xmin=0, xmax=24)
+plt.grid(visible=True, alpha=0.5, zorder=0)
+plt.xticks([i for i in range(0, 25)])
+plt.yticks([i for i in range(0, 11)])
 plt.xlabel('t')
 plt.ylabel(r'$\lambda (t)$')
-plt.plot([i for i in range(24)], lambda_t)
 plt.show()
 
-#%%  Piece wise function
-t = np.linspace(0, 24, 100)
-steps_piece_wise_function = [((t >= 0) & (t <= 5)), ((t > 5) & (t <= 6)), ((t > 6) & (t <= 7)), ((t > 7) & (t <= 9)),
-                             ((t > 9) & (t <= 10)), ((t > 10) & (t <= 11)), ((t > 11) & (t <= 12)),
-                             ((t > 12) & (t <= 14)), ((t > 14) & (t <= 17)), ((t > 17) & (t <= 20)),
-                             ((t > 20) & (t <= 24))]
+# %% Number 4
 
-return_lambda_function = [lambda t: 1, lambda t: t - 4, lambda t: 3*t - 16, lambda t: 5,
-                          lambda t: -2*t + 23, lambda t: 5*t - 47, lambda t: 2*t - 14, lambda t: 10,
-                          lambda t: -2*t + 38, lambda t: -t + 21, lambda t: 1]
-
-plt.scatter(np.linspace(0, 24, 100), np.piecewise(t, steps_piece_wise_function, return_lambda_function))
-plt.plot(np.linspace(0, 24, 100), np.piecewise(t, steps_piece_wise_function, return_lambda_function))
-plt.show()
-# %%
-formula = Piecewise((1, ((t >= 0) & (t <= 5))), (t - 4, ((t > 5) & (t <= 6))), (3*t - 16, ((t > 6) & (t <= 7))),
-                    (5, ((t > 7) & (t <= 9))), (-2*t + 23, ((t > 9) & (t <= 10))), (5*t - 47, ((t > 10) & (t <= 11))),
-                    (2*t - 14, ((t > 11) & (t <= 12))), (10, ((t > 12) & (t <= 14))), (-2*t + 38, ((t > 14) & (t <= 17))),
+formula = Piecewise((1, ((t >= 0) & (t <= 5))), (t - 4, ((t > 5) & (t <= 6))), (3 * t - 16, ((t > 6) & (t <= 7))),
+                    (5, ((t > 7) & (t <= 9))), (-2 * t + 23, ((t > 9) & (t <= 10))),
+                    (5 * t - 47, ((t > 10) & (t <= 11))),
+                    (2 * t - 14, ((t > 11) & (t <= 12))), (10, ((t > 12) & (t <= 14))),
+                    (-2 * t + 38, ((t > 14) & (t <= 17))),
                     (-t + 21, ((t > 17) & (t <= 20))), (1, ((t > 20) & (t <= 24))))
-
-
-# %%
-# non homogenous poisson process
 lambda_star = np.max(lambda_t)
+np.random.seed(32822)
 
-N = 0
-t = 0
+# %% Execute simulation
+number_arrivals_sim = Parallel(n_jobs=8)(delayed(cs2.non_homogenous_pp)(i, formula, lambda_star) for i in range(1000))
 
-uniform_variate = np.random.uniform() # Uniform(0,1)
-time_arrival_variate = - np.log(1 - uniform_variate) / lambda_star # x_n
-t = t + time_arrival_variate # t = t + x_n
+print(f'The mean of the number of arrivals for one day is: {np.mean(number_arrivals_sim)}')
+print(f'The variance of the number of arrivals for one day is: {np.var(number_arrivals_sim)}')
 
-uniform_variate_hat = np.random.uniform() # Uniform(0,1)
+# %% Number 5
 
+arrivals_and_emails = Parallel(n_jobs=8)(
+    delayed(cs2.non_homogenous_compound_pp)(i, formula, lambda_star) for i in range(10000))
